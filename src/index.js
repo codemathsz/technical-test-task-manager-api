1f2b11bcf72cf32707677fdba11f63f496be94c6
codemathsz <mo284891@gmail.com>
2025-01-09 18:20:34 -0300

    feat: route get and post task

--- src/index.js
+++ src/index.js
@@ -1,8 +1,40 @@
 const express = require('express');
+const { randomUUID } = require('crypto')
 const connection = require('./data/db')
+const cors = require('cors');
 const app = express();
-app.use(express.json)
+app.use(cors());
+app.use(express.json())
 const port = 8080;
+
+app.post('/tasks', (req, res) =>{
+  console.log("req post task");
+  
+  const { title, description } = req.body
+  if(!title || !description){
+    return res.status(400).send("Bad request: Missing title or description")
+  }
+  const task = {id: randomUUID(), title, description, status: 'pendente'}
+  const query = 'INSERT INTO tasks (id, title, description, status) VALUES (?,?,?,?)';
+  connection.query(query, [task.id, task.title, task.description, task.status], (err) =>{
+    if(err){
+      console.error('Error inserting task:', err)
+      return res.status(500).send('Internal server error')
+    }
+    return res.status(201).send(task)
+  })
+})
+
+app.get('/tasks', (req, res) => { 
+  const query = 'SELECT * FROM tasks'; 
+  connection.query(query, (err, results) => { 
+    if (err) { 
+      console.error('Error fetching tasks:', err); 
+      return res.status(500).send('Internal server error'); 
+    } 
+    res.json(results); 
+  }); 
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
